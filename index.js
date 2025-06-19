f3534b45f11930ac7c582759b3f615dde29f3ea2
SIAM632713 <siamrezwanahmed@gmail.com>
2025-06-19 17:06:32 +0600

    f98

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:[ 'http://localhost:5173','https://front-end-e-commerce-eosin.vercel.app'],
+    origin:[ 'http://localhost:5173','https://aurellia-e-commerce-eosin.vercel.app'],
     credentials: true,
 }));
 
